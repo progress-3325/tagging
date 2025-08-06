197254ed7412c93865515ceae1e8c8a0adecbc74
progress-3325 <progress.3325ml@gmail.com>
2025-08-07 01:46:39 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "tagging"
+
+tag(natural_numbers, unsigned short, unsigned int, unsigned long, unsigned long long);
+tag(whole_numbers, natural_numbers, short, int, long, long long);
+tag(numbers, whole_numbers, float, double);
+tag(char_types, char, unsigned char, signed char);
+tag(raw_types, numbers, char_types, bool);
+
+template<typename T>
+concept is_number = contains<numbers, T>;
+struct nums;
+
+tag(ext_numbers, numbers, is_number);
+
+template<typename T>
+requires contains<ext_numbers, T>
+bool is_num(const T& t)
+{
+    return true;
+}
