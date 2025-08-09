43a59a7d56bd1ff9769cc72073c1c60a55029c21
progress-3325 <progress.3325ml@gmail.com>
2025-08-09 08:29:22 +0300

    Added doxygen comments to the tag class

--- tagging.hpp
+++ tagging.hpp
@@ -4,6 +4,7 @@
 namespace std
 {
 
+    /// @brief The tag class is usually used as a static constexpr in a class, once you have it there you can check it through various compile-time operations such as concepts
     class tag
     {
         constexpr string_view id;
