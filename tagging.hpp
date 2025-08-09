71b86ae27f2da13358de2eb78b8d9623fc279e5c
progress-3325 <progress.3325ml@gmail.com>
2025-08-09 23:23:23 +0300

    Added comments documenting what constexpr and consteval keywords do

--- tagging.hpp
+++ tagging.hpp
@@ -3,13 +3,13 @@
 
 namespace std
 {
-
     /// @brief The tag class is usually used as a static constexpr in a class, once you have it there you can check it through various compile-time operations such as concepts
     class tag
     {
-        constexpr string_view id;
+        constexpr string_view id; // A constexpr variable is initialized at compile time (before main() runs) and is constant
     public:
-        consteval tag(const char* c) : id(c) {}
+        consteval tag(const char* c) : id(c) {} // A consteval function must be evaluated at compile time, the program won't compile otherwise
+                                                // if this was a constexpr function, it can run at both compile and run time.
         consteval tag(const tag& other) : id(other.get_id()) {}
         consteval string_view get_id() const {return id;}
         consteval tag& operator=(const tag& other)
@@ -21,5 +21,6 @@ namespace std
         {
             return id == c;
         }
+        consteval bool is_tag(const char* c) const {return id == c;}
     };
 }
