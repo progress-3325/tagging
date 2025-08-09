0513427b06fb5c3675a64726859209519eac6825
progress-3325 <progress.3325ml@gmail.com>
2025-08-09 08:20:17 +0300

    ditched the tag_id struct for a simple string_view

--- tagging.hpp
+++ tagging.hpp
@@ -6,17 +6,12 @@ namespace std
 
     class tag
     {
-        struct tag_id
-        {
-            constexpr string_view id;
-            consteval tag_id(const char* c) : id(c) {}
-        };
-        constexpr tag_id id("tag");
+        constexpr string_view id;
     public:
         consteval tag(const char* c) : id(c) {}
         consteval bool operator==(const char* c) const
         {
-            return id.id == c;
+            return id == c;
         }
     };
 }
